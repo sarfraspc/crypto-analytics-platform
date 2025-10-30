7a5408e9d3261838e8335ab26ea3745b32211354
sarfraspc <sarfrasspc@gmail.com>
2025-10-30 18:36:51 +0530

    changed config to parse env better on github actions

--- src/core/config.py
+++ src/core/config.py
@@ -2,10 +2,7 @@ try:
     from pydantic_settings import BaseSettings
 except ImportError:
     from pydantic import BaseSettings
-
 from typing import Dict, List
-import json
-from pydantic import field_validator
 
 class Settings(BaseSettings):
     # Postgres (metadata DB)
@@ -60,20 +57,30 @@ class Settings(BaseSettings):
     # Other
     ALTERNATIVE_ME_URL: str
     CRYPTOPANIC_URL: str
-    EXCHANGE_ADDRESSES: Dict[str, List[str]]
-
-    @field_validator('EXCHANGE_ADDRESSES', mode='before')
-    @classmethod
-    def parse_exchange_addrs(cls, v):
-        if isinstance(v, str):
-            return json.loads(v)
-        return v
-
+    EXCHANGE_ADDRESSES: Dict[str, List[str]] = {
+        "binance": [
+            "0x3f5ce5fbfe3e9af3971dd833d26ba9b5c936f0be",
+            "0xf977814e90da44bfa03b6295a0616a897441acec",
+            "0x28c6c06298d514db089934071355e5743bf21d60"
+        ],
+        "coinbase": [
+            "0xa9d1e08c7793af67e9d92fe308d5697fb81d3e43",
+            "0x71660c4005ba85c37ccec55d0c4493e66fe775d3",
+            "0x77696bb39917c91a0c3908d577d5e322095425ca",
+            "0xcd531ae9efcce479654c4926dec5f6209531ca7b"
+        ],
+        "kraken": [
+            "0xe9f7ecae3a53d2a67105292894676b00d1fab785",
+            "0x267be1c1d684f78cb4f6a176c4911b741e4ffdc0"
+        ],
+        "gemini": ["0xd24400ae8bfebb18ca49be86258a3c749cf46853"],
+        "bitfinex": [
+            "0x742d35cc6634c0532925a3b844bc454e4438f44e",
+            "0x77134cbc06cb00b66f4c7e623d5fdbf6777635ec"
+        ]
+    }
+    
     class Config:
         env_file = ".env"
-
-    @property
-    def exchange_addrs(self) -> set[str]:
-        return set(addr.lower() for addrs in self.EXCHANGE_ADDRESSES.values() for addr in addrs)
 
 settings = Settings()
